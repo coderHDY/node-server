2683be6a7ba38d159307af8b2a4fa6d6daea3c54
coderHDY <huangdeyu2020@163.com>
2022-08-20 20:50:26 +0800

    上传文件

--- routers/v1/index.js
+++ routers/v1/index.js
@@ -1,11 +1,11 @@
-const { test, uploadFile, postTest, upload, readFile } = require("./video");
+const { test, uploadFile, postTest, upload, readFile, fileReader } = require("./video");
 const express = require("express");
 const path = require("path");
 
 module.exports = (app) => {
     app.get("/", test);
     app.post("/uploadFile", upload.single("file"), uploadFile);
-    app.post("/readFile", readFile);
+    app.post("/readFile", fileReader.single("file"), readFile);
     app.post("/postTest", postTest);
     app.use("/file", express.static(path.join(__dirname, "../../files")));
 }
