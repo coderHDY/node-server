0111ef8eca0fa716e1d97e5f55ad03820834e1fc
coderHDY <huangdeyu2020@163.com>
2022-09-04 12:30:17 +0800

    node-fetch

--- routers/v1/api.js
+++ routers/v1/api.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 router.get("/robot/:val", async (req, res) => {
     const { val } = req.params;
+    console.log(`robot get ${val}`);
     try {
         const msg = await (await fetch(`http://api.qingyunke.com/api.php?key=free&appid=0&msg=${val}`)).json();
         res.send(msg);
     } catch (e) {
+        console.log(e);
         res.send({ e });
     }
 });
