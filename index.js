447391d02c6637b81b3a7be4b5e8468333ee6c6e
coderHDY <huangdeyu2020@163.com>
2022-09-04 14:11:06 +0800

    改端口

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const port = 80;
 const app = new require("express")();
 const usePlugin = require("./utils/index.js");
 const useRouter = require("./routers/v1/index.js");
 usePlugin(app);
 useRouter(app);
 
-app.listen(8888, () => console.log(`欢迎来到小黄的后端程序，正在为您转接端口：${8888}`));
+app.listen(port, () => console.log(`欢迎来到小黄的后端程序，正在为您转接端口：${port}`));
