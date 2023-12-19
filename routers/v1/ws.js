9a11fd4eb01180bb9e6c8e61236e3f116e31c613
coderHDY <986005715@qq.com>
2023-12-19 18:07:55 +0800

    feat: ws

--- routers/v1/ws.js
+++ routers/v1/ws.js
@@ -4,18 +4,20 @@ const router = express.Router();
 
 let ws;
 router.ws("/screen", (wsInstance, req) => {
-    ws = wsInstance;
-    ws.on('message', function (msg) {
-        ws.send(msg);
-    });
-})
+  console.log("------  screen ------ ");
+  ws = wsInstance;
+  ws.on("message", function (msg) {
+    ws.send(msg);
+  });
+});
 router.post("/msg", (req, res) => {
-    const msg = req.body;
-    if (!ws) {
-        return res.send({ "done": false });
-    }
-    ws.send(JSON.stringify(msg));
-    res.send({ "done": true });
-})
+  const msg = req.body;
+  console.log(msg);
+  if (!ws) {
+    return res.send({ done: false });
+  }
+  ws.send(JSON.stringify(msg));
+  res.send({ done: true });
+});
 
 module.exports = router;
